dc3bbe9783193dfdac6fc2bead5e1d23f119b463
Ladrillo <gabriel@cabrejas.net>
2021-12-14 18:53:42 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { } = require('./auth-helpers')
+const { tokenBuilder } = require('./auth-helpers')
 const router = require('express').Router()
 const User = require('../users/users-model.js')
 
@@ -28,7 +28,9 @@ router.post('/login', (req, res, next) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         // here we make this thing called token
         // it's just a string that's just as good as valid credentials
-        res.status(200).json({ message: `Welcome back ${user.username}...` })
+        const token = tokenBuilder()
+        res.status(200).json({
+          message: `Welcome back ${user.username}...`\})
       } else {
         next({ status: 401, message: 'Invalid Credentials' })
       }
