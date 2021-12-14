6e2eaba9f7a5838011a84c76c79e668d4c6a92cc
Ladrillo <gabriel@cabrejas.net>
2021-12-14 18:35:33 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -26,6 +26,8 @@ router.post('/login', (req, res, next) => {
   User.findBy({ username })
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // here we make this thing called token
+        // it's just a string that's just as good as valid credentials
         res.status(200).json({ message: `Welcome back ${user.username}...` })
       } else {
         next({ status: 401, message: 'Invalid Credentials' })
